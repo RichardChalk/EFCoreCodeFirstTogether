7c7b3d62aee1a9f69a16c6b58627fae47ce62ee4
Richard Chalk <barmybrit@gmail.com>
2022-11-23 15:28:09 +0100

    #18 Update functionality added

--- EFCoreCodeFirstTogether/Application.cs
+++ EFCoreCodeFirstTogether/Application.cs
@@ -70,10 +70,13 @@ namespace EFCoreCodeFirstTogether
                 dbContext.SaveChanges();
             }
 
-            // READ READ READ READ READ READ READ READ READ READ READ READ READ READ
+            // 16: READ READ READ READ READ READ READ READ READ READ READ READ READ READ
             using (var dbContext = new ApplicationDbContext(options.Options))
             {
-                foreach (var person in dbContext.Person.Include(c=>c.County))
+                Console.WriteLine("(R)EAD alla personer");
+                Console.WriteLine("=====================");
+
+                foreach (var person in dbContext.Person.Include(c => c.County))
                 {
                     Console.WriteLine($"Namn: {person.Name}");
                     Console.WriteLine($"Ålder: {person.Age}");
@@ -82,11 +85,54 @@ namespace EFCoreCodeFirstTogether
                     {
                         Console.WriteLine($"County kontakperson: {person.County.ContactPerson}");
                     }
-                    
+
                     Console.WriteLine("====================");
                 }
             }
 
+            // 18: UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE
+            using (var dbContext = new ApplicationDbContext(options.Options))
+            {
+                Console.WriteLine("(U)PDATE en befintlig person");
+                Console.WriteLine("=====================");
+                
+                // Vilken person ska uppdateras?
+                foreach (var person in dbContext.Person)
+                {
+                    Console.WriteLine($"Id: {person.Id}");
+                    Console.WriteLine($"Namn: {person.Name}");
+                    Console.WriteLine("====================");
+                }
+
+                Console.WriteLine("Välj Id på den Person som du vill uppdatera");
+                var personIdToUpdate = Convert.ToInt32( Console.ReadLine() );
+                var personToUpdate = dbContext.Person.First(p => p.Id == personIdToUpdate);
+
+                // Uppdatera korrekt person
+                Console.WriteLine("Ange namn: ");
+                var nameUpdate = Console.ReadLine();
+
+                Console.WriteLine("Ange ålder: ");
+                var ageUpdate = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ange skostorlek: ");
+                var shoeSizeUpdate = Convert.ToInt32(Console.ReadLine());
+
+                foreach (var county in dbContext.County)
+                {
+                    Console.WriteLine($"{county.Id} - {county.Name}");
+                }
+                Console.WriteLine("Ange Id på County");
+                var countyIdUpdate = Convert.ToInt32(Console.ReadLine());
+                var countyUpdate = dbContext.County.First(c => c.Id == countyIdUpdate);
+
+                // Mappar input info till rätt person
+                personToUpdate.Age = ageUpdate;
+                personToUpdate.Name = nameUpdate;
+                personToUpdate.ShoeSize = shoeSizeUpdate;
+                personToUpdate.County = countyUpdate;
+                dbContext.SaveChanges();
+            }
         }
     }
 }
