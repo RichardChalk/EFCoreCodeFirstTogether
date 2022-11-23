afd1ac6413ef19322f6a159f14d17a696d8d5996
Richard Chalk <barmybrit@gmail.com>
2022-11-23 14:36:45 +0100

    #15 Create a new person functionality added

--- EFCoreCodeFirstTogether/Application.cs
+++ EFCoreCodeFirstTogether/Application.cs
@@ -32,9 +32,42 @@ namespace EFCoreCodeFirstTogether
             {
                 var dataInitiaizer = new DatainItializer();
                 dataInitiaizer.MigrateAndSeed(dbContext);
-                
+
                 // Migrate har flyttats till dataInitializer class (BEST PRACTISE)
                 // dbContext.Database.Migrate();
+            }
+
+            // 15: CREATE CREATE CREATE CREATE CREATE CREATE CREATE CREATE CREATE
+            using (var dbContext = new ApplicationDbContext(options.Options))
+            {
+                Console.WriteLine("(C)REATE en ny person");
+                Console.WriteLine("=====================");
+                
+                Console.WriteLine("Ange namn: ");
+                var nameInput = Console.ReadLine();
+
+                Console.WriteLine("Ange ålder: ");
+                var ageInput = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ange skostorlek: ");
+                var shoeSizeInput = Convert.ToInt32(Console.ReadLine());
+
+                foreach (var county in dbContext.County)
+                {
+                    Console.WriteLine($"{county.Id} - {county.Name}");
+                }
+                Console.WriteLine("Ange Id på County");
+                var countyId = Convert.ToInt32(Console.ReadLine());
+                var countyInput = dbContext.County.First(c=>c.Id == countyId);
+
+                dbContext.Person.Add(new Person
+                {
+                    Age = ageInput,
+                    Name = nameInput,
+                    ShoeSize = shoeSizeInput,
+                    County = countyInput
+                });
+                dbContext.SaveChanges();
             }
         }
     }
