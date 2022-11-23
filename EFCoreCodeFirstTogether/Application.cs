36b90b1b713d167e2fece6f15bbdb9d816fabe0e
Richard Chalk <barmybrit@gmail.com>
2022-11-23 15:32:25 +0100

    #19 Added Delete functionality

--- EFCoreCodeFirstTogether/Application.cs
+++ EFCoreCodeFirstTogether/Application.cs
@@ -133,6 +133,28 @@ namespace EFCoreCodeFirstTogether
                 personToUpdate.County = countyUpdate;
                 dbContext.SaveChanges();
             }
+
+            // 19: DELETE DELETE DELETE DELETE DELETE DELETE DELETE DELETE DELETE
+            using (var dbContext = new ApplicationDbContext(options.Options))
+            {
+                Console.WriteLine("(D)ELETE en befintlig person");
+                Console.WriteLine("=====================");
+
+                // Vilken person ska raderas?
+                foreach (var person in dbContext.Person)
+                {
+                    Console.WriteLine($"Id: {person.Id}");
+                    Console.WriteLine($"Namn: {person.Name}");
+                    Console.WriteLine("====================");
+                }
+
+                Console.WriteLine("Välj Id på den Person som du vill radera");
+                var personIdToDelete = Convert.ToInt32(Console.ReadLine());
+                var personToDelete = dbContext.Person.First(p => p.Id == personIdToDelete);
+                dbContext.Person.Remove(personToDelete);
+
+                dbContext.SaveChanges();
+            }
         }
     }
 }
