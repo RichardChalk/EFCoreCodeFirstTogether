565ecc7ea54fb78a80be436498c094fecffffd95
Richard Chalk <barmybrit@gmail.com>
2022-11-23 07:40:15 +0100

    #5 Created 'Data' folder. Created classes.

---
+++ EFCoreCodeFirstTogether/Data/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirstTogether.Data
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<Invoice> Invoices { get; set; } = new List<Invoice>();
+    }
+}
