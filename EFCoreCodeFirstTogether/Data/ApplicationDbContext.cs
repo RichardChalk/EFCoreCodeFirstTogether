516c494d2a16cd9b2e528651cb7434ccf2c05e83
Richard Chalk <barmybrit@gmail.com>
2022-11-23 10:41:04 +0100

    #6 Create DBContext & options & connectionstring variables

---
+++ EFCoreCodeFirstTogether/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirstTogether.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Invoice> Invoice { get; set; }
+
+    }
+}
--- EFCoreCodeFirstTogether/Program.cs
+++ EFCoreCodeFirstTogether/Program.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using EFCoreCodeFirstTogether.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
+// Makes it possible to connect to appsettings.json
 var builder = new ConfigurationBuilder()
     .AddJsonFile($"appsettings.json", true, true);
 var config = builder.Build();
 
+// Define options and connection string
+var options = new DbContextOptionsBuilder<ApplicationDbContext>();
+var connectionString = config.GetConnectionString("DefaultConnection");
+options.UseSqlServer(connectionString);
 
 
 
